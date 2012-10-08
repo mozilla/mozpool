27e03300f713abd37efba261139197265617e10e
Dustin J. Mitchell <dustin@mozilla.com>
2012-10-08 11:41:57 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='blackmobilemagic',
+      version='1.0',
+      description='Frontend for the BMM Boot and Imaging Service',
+      author='Ted Mielczarek',
+      author_email='tmielczarek@mozilla.com',
+      package_dir={ '' : 'server' },
+      python_modules=[ 'handlers' ],
+      install_requires=[
+          'web.py',
+          # needs the git version for now; let's see if mcote can release 0.6 so we don't have to do this
+          #'git+git://github.com/markrcote/templeton.git#egg=templeton'
+      ],
+     )
