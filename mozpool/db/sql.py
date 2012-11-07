abf212a2c5d54c3dad80c656871a68cd49c5b4e5
Dustin J. Mitchell <dustin@mozilla.com>
2012-11-07 12:12:54 -0500

    bulletproof connection and engine handling

--- mozpool/db/sql.py
+++ mozpool/db/sql.py
@@ -4,19 +4,43 @@
 
 import sqlalchemy
 from mozpool import config
+import threading
 
 # global for convenience
 engine = None
 
+def _checkout_listener(dbapi_con, con_record, con_proxy):
+    try:
+        cursor = dbapi_con.cursor()
+        cursor.execute("SELECT 1")
+    except dbapi_con.OperationalError, ex:
+        if ex.args[0] in (2006, 2013, 2014, 2045, 2055):
+            raise sqlalchemy.exc.DisconnectionError()
+        raise
+
+_get_engine_lock = threading.Lock()
 def get_engine():
     """
     Get a database engine object.
     """
-    global engine
-    if engine is None:
-        engine_url = config.get('database', 'engine')
-        engine = sqlalchemy.create_engine(engine_url)
-    return engine
+    with _get_engine_lock:
+        global engine
+        if engine is None:
+            engine_url = config.get('database', 'engine')
+
+            # optimistically recycle connections after 10m
+            engine = sqlalchemy.create_engine(engine_url, pool_recycle=600)
+            # and pessimistically check connections before using them
+            sqlalchemy.event.listen(engine.pool, 'checkout', _checkout_listener)
+
+            # set sqlite to WAL mode to avoid weird concurrency issues
+            if engine.dialect.name == 'sqlite':
+                try:
+                    engine.execute("pragma journal_mode = wal")
+                except:
+                    pass # oh well..
+
+        return engine
 
 def get_conn():
     """
