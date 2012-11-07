5859f9045affed22c87e72b14c1d6f72b4447341
Dustin J. Mitchell <dustin@mozilla.com>
2012-11-07 15:38:45 -0500

    fix up setup.py to include all files

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
+print setup.func_code
 setup(name='mozpool',
       version='0.3.0',
       description='System to manage a pool of cranky mobile devices',
       author=u'Ted Mielczarek, Mark Côté, Dustin Mitchell',
+      url='http://hg.mozilla.org/build/mozpool',
       author_email='ted@mielczarek.org',
-      packages=[ 'mozpool' ],
+      packages=find_packages('.'),
       package_data={
-          'mozpool': [ 'html/*.html', 'html/css/*.css', 'html/js/*.js', 'html/js/deps/*.js' ],
+          'mozpool' : [ 'html/*.html', 'html/css/*.css', 'html/js/*.js', 'html/js/deps/*.js' ],
       },
       install_requires=[
           'sqlalchemy',
           'requests',
-          'templeton'
+          'templeton',
+          'distribute'
       ],
       entry_points={
           'console_scripts': [
