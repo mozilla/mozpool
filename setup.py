59795cdc717afee1eb5e8afb12322f70d89cb964
Dustin J. Mitchell <dustin@mozilla.com>
2012-11-20 14:51:10 -0500

    bump to 0.5.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='mozpool',
-      version='0.5.0',
+      version='0.5.1',
       description='System to manage a pool of cranky mobile devices',
       author=u'Ted Mielczarek, Mark Côté, Dustin Mitchell',
       url='http://hg.mozilla.org/build/mozpool',
