a9768f4fcac4a86f375cca8ab6a99f4b8144afcd
Mark Côté <mcote@alumni.uwaterloo.ca>
2012-11-27 15:04:24 -0500

    testdata.py now more intelligent, more closely following real world setups.

--- testdata.py
+++ testdata.py
@@ -10,6 +10,7 @@ Put some fake data into a sqlite database. Useful for testing.
 # conn is the DB connection
 
 import datetime
+import math
 import socket
 from mozpool.db import model
 from optparse import OptionParser
@@ -23,28 +24,49 @@ parser.add_option('-p', '--port', dest='port', action='store', type='int',
                   default=80)
 (options, args) = parser.parse_args(args)
 
-fqdn_with_port = fqdn = socket.getfqdn()
-if options.port != 80:
-    fqdn_with_port += ':%d' % options.port
+# max 8 relays each in max 4 banks per server
+num_servers = int(math.ceil(options.devices / 32.0))
 
-r = conn.execute(model.imaging_servers.insert(), fqdn=fqdn_with_port)
-img_svr_id = r.inserted_primary_key[0]
+if num_servers > 1:
+    print 'More than the maximum number of devices per server (32) specified.'
+    print 'Extra devices will be configured with an unreachable server.'
 
-for device_id in range(1, options.devices+1):
-    conn.execute(model.devices.insert(),
-                 name='device%d' % device_id,
-                 fqdn='device%d.fqdn' % device_id,
-                 inventory_id=1111 * device_id,
-                 state='free',
-                 state_counters='{}',
-                 mac_address='%012x' % device_id,
-                 imaging_server_id=img_svr_id,
-                 relay_info='%s:bank1:relay%d' % (fqdn, device_id),
-                 boot_config='')
+img_svr_ids = []
+fqdns = []
+
+for i in range(0, num_servers):
+    if i == 0:
+        fqdn = socket.getfqdn()
+        if options.port != 80:
+            fqdn += ':%d' % options.port
+    else:
+        fqdn = 'fakeserver%d.local' % i
+
+    fqdns.append(fqdn)
+    r = conn.execute(model.imaging_servers.insert(), fqdn=fqdn)
+    img_svr_ids.append(r.inserted_primary_key[0])
+
+device_ids = []
+
+for device_id in range(0, options.devices):
+    server_id, relay = divmod(device_id, 32)
+    bank, relay = [x + 1 for x in divmod(relay, 8)]
+    r = conn.execute(model.devices.insert(),
+                     name='device%d' % (device_id + 1),
+                     fqdn='device%d.fqdn' % (device_id + 1),
+                     inventory_id=1111 * (device_id + 1),
+                     state='free',
+                     state_counters='{}',
+                     mac_address='%012x' % device_id,
+                     imaging_server_id=img_svr_ids[server_id],
+                     relay_info='%s:bank%d:relay%d' % (fqdns[server_id], bank,
+                                                       relay),
+                     boot_config='')
+    device_ids.append(r.inserted_primary_key[0])
 
 for request_id in range(1, options.requests+1):
     conn.execute(model.requests.insert(),
-                 device_id=request_id,
+                 device_id=device_ids[request_id],
                  requested_device='any',
                  assignee='slave%d' % request_id,
                  state='new',
