efe0f04e3bb3074d21e5f8f2033aca52ce8c8357
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2013-01-25 15:44:35 -0500

    Integrate mozpoolclient 0.1.0. r=dustin

---
+++ mozpoolclient/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='mozpoolclient',
+    version='0.1.0',
+    author='Zambrano, Armen',
+    author_email='armenzg@mozilla.com',
+    packages=['mozpoolclient'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/MozpoolClient/',
+    license='MPL',
+    description='It allows you to interact with devices managed by Mozpool.',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
