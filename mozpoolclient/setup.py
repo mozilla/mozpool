19863c494c40095d8d406d456caed16cff0e83a5
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2013-01-30 15:51:35 -0500

    Bug 836449. Point to mozpool main repo and use setup from setuptools. r=dustin

--- mozpoolclient/setup.py
+++ mozpoolclient/setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='mozpoolclient',
-    version='0.1.0',
+    version='0.1.1',
     author='Zambrano, Armen',
     author_email='armenzg@mozilla.com',
     packages=['mozpoolclient'],
